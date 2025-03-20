44e77f089eeb656b09922c17649a15da68c4be31
Aakash Varma Nadimpalli <47041269+aakash4305@users.noreply.github.com>
2025-03-20 09:42:04 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/bash
 
+curl -sSf https://astral.sh/uv/install.sh | bash
+uv pip install vllm
 uv sync
-vllm serve OpenGVLab/InternVL2_5-1B-MPO --dtype=half &
-sleep 20
-uv run main.py -m OpenGVLab/InternVL2_5-1B-MPO
+source .venv/bin/activate  #virtual environment
+pkill vllm # vLLM processes have become unresponsive or are consuming too many resources
+vllm serve OpenGVLab/InternVL2_5-8B-MPO-AWQ --trust-remote-code  --quantization awq --dtype half # to connect to vllm server
+uv run main.py -m OpenGVLab/InternVL2_5-8B-MPO-AWQ --pdf-file SlamonetalSCIENCE1987.pdf --use-llm-eval #to run the main program
